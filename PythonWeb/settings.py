005cf3e25375c40f61db0f97d4f3422a879af24d
unknown <IT-DUC@vs.com>
2022-03-11 16:10:54 +0700

    blog page vs post_detail ok

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = []
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list),
-    path('<int:id>', views.post),
+    path('', views.list, name='blog'),
+    path('<int:id>', views.post, name='post'),
 ]
