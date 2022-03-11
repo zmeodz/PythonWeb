c2660949e0784dc5d01271ac41bec7440f98fb7b
unknown <IT-DUC@vs.com>
2022-03-11 12:38:05 +0700

    20220311 12h30h update admin page

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
 USE_I18N = True
 
