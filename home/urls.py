16ac4d6abacde1a762fbfc2f6cbe1c8f48dca6ac
unknown <IT-DUC@vs.com>
2022-03-12 00:12:34 +0700

    login vs logout udpate

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index),
     path('contact/', views.contact, name='contact'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.auth_login, {'template_name':'pages/login.html'}, name='login'),
-    path('logout/', auth_views.auth_logout, {'next_page':'/'}, name='logout'),
+    path('login/',auth_views.LoginView.as_view(template_name="pages/login.html"), name="login"),
+    path('logout/',auth_views.LogoutView.as_view(next_page='/'),name='logout'),
 ]
